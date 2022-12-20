b8b1c4b7e2e6cd1d33f4009fb4a0f04fd0b43b6b
priyanka1811-rk <pv012949@gmail.com>
2022-12-21 00:06:20 +0530

    chapter

---
+++ inc/header.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark main-menu">
+		  <div class="container-xl container-fluid">
+		    <a class="navbar-brand" href="#">Chapter247</a>
+		    <button class="navbar-toggler" type="button" data-bs-toggle="" data-bs-target="">
+		      <span class="navbar-toggler-icon"></span>
+		    </button>
+		    <div class="collapse navbar-collapse justify-content-end" id="">
+		      <ul class="navbar-nav">
+		        <li class="nav-item">
+		          <a class="nav-link" href="index.php">Home</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#about">About</a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#feature">Features</a>
+		        </li> 
+		        <li class="nav-item">
+		          <a class="nav-link" href="#price">Pricing</a>
+		        </li> 
+		        <li class="nav-item">
+		          <a class="nav-link" href="#team">Team</a>
+		        </li>   
+		        <li class="nav-item dropdown">
+		          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Drop Down</a>
+		          <ul class="dropdown-menu">
+		            <li><a class="dropdown-item" href="#">Option1</a></li>
+		            <li><a class="dropdown-item" href="#">Option2</a></li>
+		            <li><a class="dropdown-item" href="#">Option3</a></li>
+		          </ul>
+		        </li> 
+		        <li class="nav-item">
+		          <a class="nav-link" href="#contact">Contact Us</a>
+		        </li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
